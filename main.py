5e80ea4889b869e189210d4457ae8dc802fd7beb
jqjiang <jjqzy@foxmail.com>
2018-10-13 15:32:22 +0800

    main function modified to apply multi-stocks test

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from crawler import Crawler
 from mongodb import MongoDb
 
-stock_id = 600519
-year_start = 2016
-year_end = 2017
+stock_ids = [600519, 600520, 600521]
+year_start, year_end = 2016, 2017
 
 
 def main():
     crawler = Crawler()
     db = MongoDb()
 
-    data = crawler.fetch_stock_data(stock_id, year_start, year_end)
-    db.append_one(data)
+    for stock_id in stock_ids:
+        data = crawler.fetch_stock_data(stock_id, year_start, year_end)
+        db.append_one(data)
 
 
 if __name__ == '__main__':
